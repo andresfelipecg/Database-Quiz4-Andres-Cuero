ef12cafa25634b520ea07a3d039d469bba6a5d40
Andres Cuero <andresfelipecuero@gmail.com>
2023-04-14 17:05:00 -0400

    add Database access

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//Andres Cuero
+//301203787
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Database access has already been done on April 14, 2023 by Andres Cuero- 301203787");
+
+        // Wait for user input before closing console window
+        Console.ReadLine();
+    }
+}
